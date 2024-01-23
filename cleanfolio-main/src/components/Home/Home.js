9a7c87c1b63fed9f0c84caed27489d6823eeb0e5
unknown <you@example.com>
2024-01-24 00:09:22 +0530

    Profile photo changed

--- cleanfolio-main/src/components/Home/Home.js
+++ cleanfolio-main/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { home } from '../../portfolio'
 import './Home.css'
-import IMAGE from './photostudio-8-designify.png'
+import IMAGE from './ResumeProfilePhotos.png'
 
 function Home() {
   const { name, role, resume, social } = home
