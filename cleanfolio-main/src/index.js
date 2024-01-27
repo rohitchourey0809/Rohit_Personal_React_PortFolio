861b4f1c6ed7072986e124cda876663b8fda5aed
unknown <you@example.com>
2024-01-27 16:04:20 +0530

    fixed issue render

--- cleanfolio-main/src/index.js
+++ cleanfolio-main/src/index.js
@@ -1,15 +1,16 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
 
-render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ThemeProvider>,
 
-  document.getElementById('root')
+
 )
