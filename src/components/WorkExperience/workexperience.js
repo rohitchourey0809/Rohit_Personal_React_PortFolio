fa20207cf600175419d131a4fa22665e6e5860d7
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2025-01-23 10:19:54 +0530

    Ziptrrip Experience added

--- src/components/WorkExperience/workexperience.js
+++ src/components/WorkExperience/workexperience.js
@@ -11,10 +11,10 @@ const WorkExperienceSummary = () => {
     <>
       <section className='section aboutsummary' id='experience'>
         <h2 className='section__title'>Work Experience</h2>
-        <h3 className='section__title'>Indus Net Technologies </h3>
-        <h3 className='section__title'>Sep 2022 - Feb 2024 | Kolkata </h3>
+        <h3 className='section__title'>Ziptrrip.com</h3>
+        <h3 className='section__title'>July 2024 - Current | Mumbai </h3>
         <ul className='aboutsummary__list aboutsummary__list-item btn btn--plain experience'>
-          {WorkExperience.map((e) => (
+          {WorkExperienceZiptrrip.map((e) => (
             <div key={uniqid()} className='bullet-list'>
               <li>{e}</li>
             </div>
@@ -23,10 +23,10 @@ const WorkExperienceSummary = () => {
       </section>
       <section className='section aboutsummary' id='experience'>
         <h2 className='section__title'>Work Experience</h2>
-        <h3 className='section__title'>Ziptrrip.com</h3>
-        <h3 className='section__title'>July 2024 - Current | Mumbai </h3>
+        <h3 className='section__title'>Indus Net Technologies </h3>
+        <h3 className='section__title'>Sep 2022 - Feb 2024 | Kolkata </h3>
         <ul className='aboutsummary__list aboutsummary__list-item btn btn--plain experience'>
-          {WorkExperienceZiptrrip.map((e) => (
+          {WorkExperience.map((e) => (
             <div key={uniqid()} className='bullet-list'>
               <li>{e}</li>
             </div>
